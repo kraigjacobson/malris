c922b712ccf6a3ed277281fc28b2199fcf0a9cfa
Kraig Jacobson <kraig.jacobson@gmail.com>
2025-06-27 11:21:00 -0400

    Fix malris.kraig.io bad gateway: disable SSR and update deployment workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  ssr: false,
   modules: [
     '@nuxt/icon',
     '@nuxt/eslint',
     '@nuxt/ui',
     '@nuxtjs/supabase',
-    '@nuxt/content',
     '@nuxt/image',
     '@pinia/nuxt'
   ],
